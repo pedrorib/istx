c5744538a49bee64e467c9e3f460628ec7f98503
pedrorib <pedro_rib2@hotmail.com>
2015-09-17 10:38:49 +0100

    Script de instalação actualizado.

--- sandbox.sh
+++ sandbox.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 ##
-## Installs the pre-requisites for running edX on a single Ubuntu 12.04
-## instance.  This script is provided as a convenience and any of these
-## steps could be executed manually.
-##
-## Note that this script requires that you have the ability to run
-## commands as root via sudo.  Caveat Emptor!
-##
 
-##
-## Sanity check
-##
+
+export OPENEDX_RELEASE=named-release/cypress
+CONFIG_VER=$OPENEDX_RELEASE
+
 if [[ ! "$(lsb_release -d | cut -f2)" =~ $'Ubuntu 14.04.2 LTS' ]]; then
    echo "Esta versão da configuração foi modificada para trabalhar somente com Ubuntu 14.04.2 LTS. Abortar";
    exit;
@@ -45,8 +39,26 @@ echo "2" >> ~/progress.txt
 if [["$(cat ~/progress.txt | tail --lines=1)"="2"]]; then
  echo "SKIPPING"
 else
-cd /var/tmp
-git clone https://github.com/pedrorib/istx.git
+	cd /var/tmp
+	git clone https://github.com/edx/configuration
+	cd configuration
+	git checkout $CONFIG_VER
+	
+   if [ -n "$OPENEDX_RELEASE" ]; then
+     EXTRA_VARS="-e edx_platform_version=$OPENEDX_RELEASE \
+       -e certs_version=$OPENEDX_RELEASE \
+       -e forum_version=$OPENEDX_RELEASE \
+       -e xqueue_version=$OPENEDX_RELEASE \
+     "
+     CONFIG_VER=$OPENEDX_RELEASE
+   else
+     CONFIG_VER="named-release/cypress"
+   fi
+
+sed -i "/libblas/ s/^/#/g" /var/tmp/configuration/playbooks/roles/edxapp/tasks/python_sandbox_env.yml
+sed -i "/liblapack/ s/^/#/g" /var/tmp/configuration/playbooks/roles/edxapp/tasks/python_sandbox_env.yml
+sed -i "s/https/http/g" /var/tmp/configuration/playbooks/roles/elasticsearch/defaults/main.yml
+
 fi
 
 echo "3" >> ~/progress.txt
@@ -58,19 +70,12 @@ echo "3" >> ~/progress.txt
 if [["$(cat ~/progress.txt | tail --lines=1)"="3"]]; then
  echo "SKIPPING"
 else
-cd /var/tmp/istx
+cd /var/tmp/configuration
 sudo pip install -r requirements.txt
 fi
-
-echo "4" >> ~/progress.txt
 
 ##
 ## Run the edx_sandbox.yml playbook in the configuration/playbooks directory
 ##
-if [["$(cat ~/progress.txt | tail -lines=1)"="4"]]; then
- echo "SKIPPING"
-else
-cd /var/tmp/istx/playbooks && sudo ansible-playbook -c local ./edx_sandbox.yml -i "localhost," $EXTRA_VARS
-fi
 
-rm ~/progress.txt
+cd /var/tmp/configuration/playbooks && sudo ansible-playbook -c local ./edx_sandbox.yml -i "localhost," $EXTRA_VARS
